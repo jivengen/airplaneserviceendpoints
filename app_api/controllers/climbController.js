522a76c6d40fde7fc86c34aadee3470654781262
coolbule64 <jivengen1@buffs.wtamu.edu>
2017-11-02 02:50:19 +0000

    queries 2.0

--- app_api/controllers/climbController.js
+++ app_api/controllers/climbController.js
@@ -3,7 +3,24 @@ var climbData = mongoose.model('climbdata');
 
 module.exports.findclimb = function(req, res, next) {
     var weight = req.params.weight;
-    climbData.find({"weight" : weight}, function (err,climbdatas){
+    if(weight < 19000){
+        weight = 18000;
+    }else if(weight < 21000){
+        weight = 20000;
+    }else if(weight < 23000){
+        weight = 22000;
+    }else if(weight < 25000){
+        weight = 24000;
+    }else if(weight < 27000){
+        weight = 26000;
+    }else if(weight < 28500){
+        weight = 28000;
+    }else{
+        weight = 29000;
+    };
+    
+    
+    climbData.find({"weight" : weight},{_id:0}, function (err,climbdatas){
         if(err){
             return console.error(err);
         }else{
